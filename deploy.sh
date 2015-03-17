2af46109ef37130d6c0bf0cce54a05250e4fb9e6
swem <r97922120@ntu.edu.tw>
2015-03-17 13:06:07 +0800

    add pam_environment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/pam_environment ~/.pam_environment
 ln -sf ~/dotfiles/.gitconfig ~
 ln -sf ~/dotfiles/.gitignore ~
 git clone https://github.com/swem/tmux-config
