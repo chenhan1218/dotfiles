f73de3fc9f1f4ea902df278ec0001f9db8b290a1
swem <r97922120@ntu.edu.tw>
2015-05-24 17:05:01 +0800

    use basic tmux.conf and vimrc

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-ln -sf ~/dotfiles/bashrc ~/.bashrc
-ln -sf ~/dotfiles/pam_environment ~/.pam_environment
-ln -sf ~/dotfiles/.gitconfig ~
-ln -sf ~/dotfiles/.gitignore ~
-git clone https://github.com/swem/tmux-config
-ln -sf ~/tmux-config/.tmux.conf ~
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ln -sf $DIR/bashrc ~/.bashrc
+ln -sf $DIR/pam_environment ~/.pam_environment
+ln -sf $DIR/.gitconfig ~
+ln -sf $DIR/.gitignore ~
+ln -sf $DIR/tmux.conf ~/.tmux.conf
+git clone https://github.com/swem/tmux-config && ln -sf ~/tmux-config/.tmux.conf ~/.tmux.conf
+ln -sf $DIR/vimrc ~/.vimrc
+git clone https://github.com/swem/vimrc ~/.vim && ln -sf ~/.vim/vimrc ~/.vimrc
 
