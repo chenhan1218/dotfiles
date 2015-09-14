551eb66492dc3478b98d9d4290d0237bf5788ed3
swem <r97922120@ntu.edu.tw>
2015-09-14 12:21:28 +0800

    add gdbinit

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ ln -sf $DIR/bashrc ~/.bashrc
 ln -sf $DIR/pam_environment ~/.pam_environment
 ln -sf $DIR/.gitconfig ~
 ln -sf $DIR/.gitignore ~
+ln -sf $DIR/bazaar ~/.bazaar
+ln -sf $DIR/gdbinit ~/.gdbinit
 ln -sf $DIR/tmux.conf ~/.tmux.conf
 git clone https://github.com/swem/tmux-config && ln -sf ~/tmux-config/.tmux.conf ~/.tmux.conf
 ln -sf $DIR/vimrc ~/.vimrc
